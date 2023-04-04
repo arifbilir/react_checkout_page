8b5bc1d633869566c27584b4dcee3c74cbed53b7
arifbilir <arifbilirde@gmail.com>
2023-04-04 15:19:40 +0200

    cardtotal

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -8,14 +8,17 @@ import axios from "axios";
 
 const ProductList = () => {
   const url = process.env.REACT_APP_API_URL;
-
   const [products,setProducts]= useState([])
+  const[loading,setLoading] = useState (true)
 
   const getProducts = async () => {
     
     try{
       const { data } = await axios(url)
       setProducts(data)
+      setLoading (false)
+      console.log(data)
+
     } catch (error) {
       console.log(error)
 
@@ -30,18 +33,20 @@ const ProductList = () => {
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-        <p className="text-center text-danger w-100">Loading....</p>
-
+        {loading ? <p className="text-center text-danger w-100">Loading....</p>
+        : products.length > 0 ?
         <>
           <article id="product-panel" className="col-md-5">
-            <ProductCard />
+            {products.map(item=>{
+              return <ProductCard key={item.id} item={item}/>;
+            }) }
           </article>
           <article className="col-md-5 m-3">
-            <CardTotal />
+            <CardTotal products={products} />
           </article>
         </>
-
-        <p className="text-center text-danger w-100">No products data...</p>
+        :
+        <p className="text-center text-danger w-100">No products data...</p>}
       </div>
     </div>
   );
